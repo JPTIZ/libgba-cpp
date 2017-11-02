30d3c9c1999f377f847025ec8cbf7f67ae1a83e3
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2017-11-02 02:14:56 -0200

    visual improvements

--- tests/mode5/main.cpp
+++ tests/mode5/main.cpp
@@ -10,14 +10,15 @@ void fill_screen(const gba::display::Color& color) {
 int main() {
     using namespace gba::display;
 
+    force_blank(true);
     change_mode(Mode::MODE5);
     layer_visible(Layer::BG2);
 
+    fill_screen(Color{31, 0, 0});
+    mode5::flip_pages();
     force_blank(false);
 
-    fill_screen(Color{31, 0, 0});
-
-    auto first = true;
+    auto first = false;
     while (true) {
         vsync();
         first = not first;
